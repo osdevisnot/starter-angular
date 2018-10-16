7f7d1ea6992032b0a88051931825190c8cf22a3d
osdevisnot <osdevisnot@gmail.com>
2018-10-15 22:27:48 -0700

    move to npm

--- setup.js
+++ setup.js
@@ -37,7 +37,7 @@ files.forEach(file => fs.unlinkSync(fromRoot(file)))
 /**
  * Add latest devDependencies and initialize git repo
  */
-const commands = ['yarn', 'git add .', 'git commit -am "first commit from starter-angular"']
+const commands = ['git add .', 'git commit -am "first commit from starter-angular"', 'npm install']
 commands.forEach(command => {
   console.log(`----- Executing Command -----> ${command}`)
   sync(command, { stdio: [0, 1, 2] })
